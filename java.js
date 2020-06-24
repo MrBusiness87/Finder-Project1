3f36f621eea9801dc809a4ba680ac3b0129bfb27
MrBusiness87 <bbartula87@gmail.com>
2020-06-23 21:38:51 -0400

    06/23/2020-finished navbar functionality.

--- java.js
+++ java.js
@@ -1,2 +1,7 @@
 //-- Google Maps API Key
 var APIkey = "AIzaSyBb0CDUuXsKE2EwQDS79oQZXtUoAA77HXc";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
