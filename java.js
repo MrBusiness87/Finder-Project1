9c87deb8ea638818fe511c8f8380b7afb31ba82b
ctbeam <61569132+ctbeam@users.noreply.github.com>
2020-06-28 19:19:12 -0400

    Added the code as statements
    instead of actual code.

--- java.js
+++ java.js
@@ -87,3 +87,44 @@ function cityWeather() {
       cityWeather();
     });
 }
+
+//function initMap(lat, lng, targetIndex) {
+    // The location of event
+    // var latLng = { lat: lat, lng: lng };
+    //var latlng = new google.maps.LatLng(lat, lng);
+    // The map, centered at city venue
+    //console.log(document.getElementById(`map${targetIndex}`));
+    //var map = new google.maps.Map(document.getElementById(`map${targetIndex}`), {
+        //zoom: 11,
+        //center: latlng
+    //});
+    // The marker, positioned at event venue
+    //var marker = new google.maps.Marker({
+        //position: latlng,
+        //map: map
+    //});
+//}
+
+//function embedTheMap(index) {
+    //var queryURL =
+        //"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sport&city=" +
+        //city +
+        //"&apikey=" +
+        //APIkey;
+
+    //$.ajax({
+        //url: queryURL,
+        //method: "GET"
+    //}).then(function(response) {
+        //var venue = response._embedded.events[index]._embedded.venues[0];
+        //var latitude = venue.location.latitude;
+        //var longitude = venue.location.longitude;
+
+        //var lat = parseFloat(latitude);
+        //var lng = parseFloat(longitude);
+        //console.log("Lat & Lng", lat, lng);
+        //initMap(lat, lng, index);
+    //});
+//}
+
+
